d3debdf34d59ad38aa11f2e264b54f1ec0c4be2b
czcll49 <xiaobin.chen@maikeos.com>
2020-11-30 14:50:13 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import AntD from 'ant-design-vue'
+import Axios from 'axios'
 import App from './App.vue'
+import 'ant-design-vue/dist/antd.css'
+
+Vue.prototype.$axios = Axios;
 
 Vue.config.productionTip = false
+Vue.use(AntD)
 
+/* eslint-disable no-new */
 new Vue({
   render: h => h(App),
 }).$mount('#app')
